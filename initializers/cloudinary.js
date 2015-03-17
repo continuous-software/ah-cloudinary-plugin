d2a3520fe5665367680602abc1f20ab6346c7079
Amine Mouafik <amine@mouafik.fr>
2015-03-17 14:13:51 +0700

    Upgrade initializer structure to AH10.

--- initializers/cloudinary.js
+++ initializers/cloudinary.js
@@ -1,13 +1,18 @@
-'use strict';
+'use strict'
 
-var Promise = require('bluebird');
-var Cloudinary = require('cloudinary');
+var Promise = require('bluebird')
+var Cloudinary = require('cloudinary')
 
-exports.cloudinary = function(api, next) {
-
-  api.cloudinary = Cloudinary;
-  api.cloudinary.config(api.config.cloudinary);
-
-  next();
-
-};
+module.exports = {
+  initialize: function (api, next) {
+    api.cloudinary = Cloudinary
+    api.cloudinary.config(api.config.cloudinary)
+    next()
+  },
+  start: function (api, next) {
+    next()
+  },
+  stop: function (api, next) {
+    next()
+  }
+}
